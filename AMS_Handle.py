b36208344b44f5a7a2a254a488da7e85940e69ce
Minh NCQ <minhncq99@gmail.com>
2021-06-13 14:24:55 +0700

    Make traning model

--- AMS_Handle.py
+++ AMS_Handle.py
@@ -63,6 +63,168 @@ def training():
     Notification.configure(text=res, bg="SpringGreen3", width=50, font=('times', 18, 'bold'))
     Notification.place(x=250, y=400)
 
+# Choose subject and fill attendance
+def subjectchoose():
+    def Fillattendances():
+        sub = tx.get()
+        now = time.time()  ###For calculate seconds of video
+        future = now + 20
+        if time.time() < future:
+            if sub == '':
+                err_screen("Sub is null")
+            else:
+                recognizer = cv2.face.LBPHFaceRecognizer_create()  # cv2.createLBPHFaceRecognizer()
+                try:
+                    recognizer.read("TrainingImageLabel\Trainner.yml")
+                except:
+                    error_screen("Model not found,Please train model")
+
+
+                harcascadePath = "haarcascade_frontalface_default.xml"
+                faceCascade = cv2.CascadeClassifier(harcascadePath)
+                df = pd.read_csv("StudentDetails\StudentDetails.csv")
+                cam = cv2.VideoCapture(0)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                col_names = ['Enrollment', 'Name', 'Date', 'Time']
+                attendance = pd.DataFrame(columns=col_names)
+                while True:
+                    ret, im = cam.read()
+                    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+                    faces = faceCascade.detectMultiScale(gray, 1.2, 5)
+                    for (x, y, w, h) in faces:
+                        global Id
+
+                        Id, conf = recognizer.predict(gray[y:y + h, x:x + w])
+                        if (conf < 70):
+                            print(conf)
+                            global Subject
+                            global aa
+                            global date
+                            global timeStamp
+                            Subject = tx.get()
+                            ts = time.time()
+                            date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+                            timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+                            aa = df.loc[df['Enrollment'] == Id]['Name'].values
+                            global tt
+                            tt = str(Id) + "-" + aa
+                            En = '15624031' + str(Id)
+                            attendance.loc[len(attendance)] = [Id, aa, date, timeStamp]
+                            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 260, 0), 7)
+                            cv2.putText(im, str(tt), (x + h, y), font, 1, (255, 255, 0,), 4)
+
+                        else:
+                            Id = 'Unknown'
+                            tt = str(Id)
+                            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 25, 255), 7)
+                            cv2.putText(im, str(tt), (x + h, y), font, 1, (0, 25, 255), 4)
+                    if time.time() > future:
+                        break
+
+                    attendance = attendance.drop_duplicates(['Enrollment'], keep='first')
+                    cv2.imshow('Filling attedance..', im)
+                    key = cv2.waitKey(30) & 0xff
+                    if key == 27:
+                        break
+
+                ts = time.time()
+                date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+                timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+                Hour, Minute, Second = timeStamp.split(":")
+                fileName = "Attendance/" + Subject + "_" + date + "_" + Hour + "-" + Minute + "-" + Second + ".csv"
+                attendance = attendance.drop_duplicates(['Enrollment'], keep='first')
+                print(attendance)
+                attendance.to_csv(fileName, index=False)
+
+                ##Create table for Attendance
+                date_for_DB = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d')
+                DB_Table_name = str(Subject + "_" + date_for_DB + "_Time_" + Hour + "_" + Minute + "_" + Second)
+                import pymysql.connections
+
+                ###Connect to the database
+                try:
+                    global cursor
+                    connection = pymysql.connect(host='localhost', user='root', password='', db='Face_reco_fill')
+                    cursor = connection.cursor()
+                except Exception as e:
+                    print(e)
+
+                sql = "CREATE TABLE " + DB_Table_name + """
+                (ID INT NOT NULL AUTO_INCREMENT,
+                 ENROLLMENT varchar(100) NOT NULL,
+                 NAME VARCHAR(50) NOT NULL,
+                 DATE VARCHAR(20) NOT NULL,
+                 TIME VARCHAR(20) NOT NULL,
+                     PRIMARY KEY (ID)
+                     );
+                """
+                ####Now enter attendance in Database
+                insert_data = "INSERT INTO " + DB_Table_name + " (ID,ENROLLMENT,NAME,DATE,TIME) VALUES (0, %s, %s, %s,%s)"
+                VALUES = (str(Id), str(aa), str(date), str(timeStamp))
+                try:
+                    cursor.execute(sql)  ##for create a table
+                    cursor.execute(insert_data, VALUES)  ##For insert data into table
+                except Exception as ex:
+                    print(ex)  #
+
+                M = 'Attendance filled Successfully'
+                Notifica.configure(text=M, bg="Green", fg="white", width=33, font=('times', 15, 'bold'))
+                Notifica.place(x=20, y=250)
+
+                cam.release()
+                cv2.destroyAllWindows()
+
+                import csv
+                import tkinter
+                root = tkinter.Tk()
+                root.title("Attendance of " + Subject)
+                root.configure(background='snow')
+                cs = const.PROJECT_PATH + fileName
+                with open(cs, newline="") as file:
+                    reader = csv.reader(file)
+                    r = 0
+
+                    for col in reader:
+                        c = 0
+                        for row in col:
+                            # i've added some styling
+                            label = tkinter.Label(root, width=8, height=1, fg="black", font=('times', 15, ' bold '),
+                                                  bg="lawn green", text=row, relief=tkinter.RIDGE)
+                            label.grid(row=r, column=c)
+                            c += 1
+                        r += 1
+                root.mainloop()
+                print(attendance)
+
+    ###windo is frame for subject chooser
+    windo = tk.Tk()
+    windo.iconbitmap('AMS.ico')
+    windo.title("Enter subject name...")
+    windo.geometry('580x320')
+    windo.configure(background='snow')
+    Notifica = tk.Label(windo, text="Attendance filled Successfully", bg="Green", fg="white", width=33,
+                        height=2, font=('times', 15, 'bold'))
+
+    def Attf():
+        import subprocess
+        subprocess.Popen(
+            r'explorer /select,"' + const.PROJECT_PATH + 'Attendance/-------Check atttendance-------"')
+
+    attf = tk.Button(windo, text="Check Sheets", command=Attf, fg="black", bg="lawn green", width=12, height=1,
+                     activebackground="Red", font=('times', 14, ' bold '))
+    attf.place(x=430, y=255)
+
+    sub = tk.Label(windo, text="Enter Subject", width=15, height=2, fg="white", bg="blue2",
+                   font=('times', 15, ' bold '))
+    sub.place(x=30, y=100)
+
+    tx = tk.Entry(windo, width=20, fg="red", font=('times', 23, ' bold '))
+    tx.place(x=250, y=105)
+
+    fill_a = tk.Button(windo, text="Fill Attendance", fg="black", command=Fillattendances, bg="plum",
+                       width=20, height=2, activebackground="violet", font=('times', 15, ' bold '))
+    fill_a.place(x=250, y=160)
+    windo.mainloop()
 
 # ----------------------Interface----------------------
 ## Window is our Main frame of system
